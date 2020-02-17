73750d7803b06888bda325e04025bde60ded8d5d
Takashi Saito <takashi11171117@gmail.com>
2020-02-17 11:00:42 +0900

    Added jwt auth

--- laravel/app/Http/Controllers/Auth/LoginController.php
+++ laravel/app/Http/Controllers/Auth/LoginController.php
@@ -3,37 +3,32 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use App\Http\Requests\Auth\LoginRequest;
+use App\User;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
+    public function action(LoginRequest $request) {
+        $credentials = $request->only('email', 'password');
 
-    use AuthenticatesUsers;
+        if (!$token = auth("api")->attempt($credentials)) {
+            return response()->json([
+                'errors' => [
+                    'email' => ['メールアドレスかパスワードが一致しません']
+                ]
+            ], 422);
+        }
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
+        return $this->respondWithToken($token, $credentials);
+    }
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    protected function respondWithToken($token)
     {
-        $this->middleware('guest')->except('logout');
+        return response()->json([
+            'token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth("api")->factory()->getTTL() * 60,
+        ]);
     }
 }
---
+++ laravel/app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Auth\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'required|email|unique:users,email',
+            'name' => 'required|unique:users,name|regex:/^[0-9a-zA-Z_\.\-]+$/',
+            'password' => 'required|confirmed|regex:/\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{8,}+\z/',
+        ];
+    }
+}
